fe9f69349b16adcb1a767b9bb8c9753e8bc9a3db
philFernandez <norcal618@gmail.com>
2021-04-10 23:54:49 -0700

    fix month

--- remind/richcontent/rich_renderables.py
+++ remind/richcontent/rich_renderables.py
@@ -42,7 +42,7 @@ class ReminderTable:
                     f"{reminder.id}",
                     reminder.description,
                     f"{reminder.entry_date.strftime('%D')}",
-                    f"{reminder.entry_date.strftime('%I:%m %p')}",
+                    f"{reminder.entry_date.strftime('%I:%M %p')}",
                 )
         elif self.verbose >= 2:
             self.table.add_column("Date")
@@ -55,7 +55,7 @@ class ReminderTable:
                     f"{reminder.id}",
                     reminder.description,
                     f"{reminder.entry_date.strftime('%D')}",
-                    f"{reminder.entry_date.strftime('%I:%m %p')}",
+                    f"{reminder.entry_date.strftime('%I:%M %p')}",
                     f"{tags}",
                 )
         else:
