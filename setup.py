f0f2076e9fe2c062b7bb65de859f7d8f94342e46
philFernandez <norcal618@gmail.com>
2021-04-15 19:37:05 -0700

    adjust setup.py for pypi

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@ from os import path
 
 project_root = path.abspath(path.dirname(__file__))
 
-with open(path.join(project_root, "README.md"), encoding="utf-8") as f:
+with open(path.join(project_root, "PYPI_README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="rmind",
-    version="v0.1-alpha",
+    version="0.1a1",
     description="A note taking cli",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Phil Fernandez",
     author_email="philfernandez@protonmail.com",
     url="https://github.com/philFernandez/rmind",
-    download_url="https://github.com/philFernandez/rmind/archive/refs/tags/v0.1-alpha.tar.gz",
-    keywords=["v0.1-alpha", "pre-release"],
+    keywords=["pre-release", "alpha", "note taking cli"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -25,7 +24,7 @@ setup(
     packages=find_packages(),
     license="MIT",
     include_package_data=True,
-    install_requires=["Click", "rich", "sqlalchemy", "python-dotenv"],
+    install_requires=["Click", "rich==10.1.0", "sqlalchemy", "python-dotenv"],
     python_requires=">=3.7",
     entry_points="""
         [console_scripts]
